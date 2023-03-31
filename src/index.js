d0d9b9f129c1baf9de7c160b0578dbe7e62989e9
pra78 <66877961+pra78@users.noreply.github.com>
2023-03-31 14:56:25 +0300

    on empty search request return

--- src/index.js
+++ src/index.js
@@ -2,14 +2,15 @@ import { Notify } from "notiflix";
 import { fetchImages } from "./js/request";
 import templateFunction from './template/template.hbs';
 
-const galleryEl = document.querySelector('.gallery');
 const form = document.querySelector('#search-form');
+const galleryEl = document.querySelector('.gallery');
 
 form.addEventListener('submit', onSubmitBtnPressed);
 
 function onSubmitBtnPressed(event) {
     event.preventDefault();
     const searchRequest = event.currentTarget.elements['searchQuery'].value;
+    if (searchRequest === '') return;
     fetchImages(searchRequest)
         .then(result => {
             if (result.hits.length === 0) {
